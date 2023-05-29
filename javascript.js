ee5b6a2d7fbb900743f7a07cc6872c6084a35466
Pyrevilo <oliverliangwork@gmail.com>
2023-05-29 18:03:14 +0930

    Dom event working

--- javascript.js
+++ javascript.js
@@ -1,18 +1,33 @@
+const btn = document.querySelectorAll('.choice');
+const choices = ["rock", "paper", "scissor"];
+
+btn.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelection = button.id;
+        computerSelection = choices[Math.floor(Math.random()*choices.length)];
+        playRound(playerSelection, computerSelection);
+        console.log("computer = "+computerSelection);
+    })
+})
+
 function playRound (playerSelection, computerSelection) {
-    const choices = ["rock", "paper", "scissors"];
-    playerSelecton = prompt("what would you like to play?");
-    var lose = "You Lose!";
-    var win = "You Win!";
-    var draw = "You Draw!";
-    computerSelection = choices[Math.floor(Math.random()*choices.length)];
-    if (playerSelection == choices[0] && computerSelection == choices[1]) {
-        return lose;
+    const output = document.querySelector('#output');
+    const content = document.createElement('div');
+    content.classList.add('content');
+    if ((playerSelection == choices[0] && computerSelection == choices[1]) || 
+        (playerSelection == choices[1] && computerSelection == choices[2]) ||
+        (playerSelection == choices[2] && computerSelection == choices[0])) {
+        content.textContent = ("winner");
+        output.appendChild(content);
     }
-    //run all conditional statements to achieve result. Inefficient but gets the job done.
-    //consider a better way to solve it later
-
+    if ((playerSelection == choices[0] && computerSelection == choices[2]) ||
+        (playerSelection == choices[1] && computerSelection == choices[0]) ||
+        (playerSelection == choices[2] && computerSelection == choices[1])) {
+        console.log("Winner");
+        return;
+    }
+    else {
+        console.log("Tie");
+        return;
+    }
 }
-
-const playerSelection = "rock";
-const computerSelection = getCompChoice();
-console.log(playRound(playerSelection, computerSelection));
