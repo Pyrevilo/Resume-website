3548cae4f43d129d7e192bc57165d90649669b85
Pyrevilo <oliverliangwork@gmail.com>
2023-06-02 11:53:01 +0930

    added draw feature

--- eas.js
+++ eas.js
@@ -1,5 +1,6 @@
 //selects grid div
 const containerGrid = document.querySelector('.grid');
+var pixel = document.getElementsByClassName('pixel');
 
 
 for (let i =0; i <= 16; i++) {
@@ -11,8 +12,16 @@ for (let i =0; i <= 16; i++) {
     for (let k = 0; k <=16; k++) {
         const gridpixel = document.createElement('div');
         gridpixel.classList.add('pixel');
-        gridpixel.textContent = k;
+        //gridpixel.textContent = k;
 
         grid.appendChild(gridpixel);
+        }
+    }
+
+for (let i = 0; i <289; i++){
+    pixel[i].addEventListener('mouseover', trail, false);
+    function trail() {
+        pixel[i].setAttribute("style", "background-color:orange");
     }
 }
+
