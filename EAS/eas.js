5826b9691876e4732209f8339b8c025e8af20207
Pyrevilo <oliverliangwork@gmail.com>
2023-06-16 16:24:54 +0930

    finished

--- EAS/eas.js
+++ EAS/eas.js
@@ -1,10 +1,11 @@
 let color = 'black';
+let click = false;
 
 //create grid
 function createGrid (size) {
     let board = document.querySelector(".grid");
     //removes existing divs to be replaced when creating grid
-    let squares = document.querySelectorAll("div");
+    let squares = board.querySelectorAll("div");
     squares.forEach((div) => div.remove());
     //sets board style to grid-template-columns/rows in css
     //this makes grid fit within constraints of field size (500px^2)
@@ -16,7 +17,7 @@ function createGrid (size) {
     for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
         square.addEventListener('mouseover', colorSquare)
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -35,10 +36,29 @@ function changeSize(input) {
 
 //sets color of drawing tool
 function colorSquare() {
-    //this refers to the div the event listener is attached to.
-    this.style.backgroundColor = color;
+    if (click) {
+        //this refers to the div the event listener is attached to.
+        this.style.backgroundColor = color;
+    }
 }
 
+//changes color of pen
 function changeColor(choice) {
     color = choice;
 }
+
+//resets board
+function resetBoard() {
+    let board = document.querySelector(".grid");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+}
+
+//only color in when holding down mouse 
+document.querySelector("body").addEventListener('mousedown', () => {
+    click = true;
+})
+
+document.querySelector("body").addEventListener('mouseup', () => {
+    click = false;
+})
